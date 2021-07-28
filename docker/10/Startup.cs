3aaa3826a4aa5dd82afb47d353578417e1d10607
Łukasz Kałużny <lukasz@kaluzny.pro>
2021-07-28 12:32:38 +0000

    Dockerfile dotnet demo

--- docker/10/Startup.cs
+++ docker/10/Startup.cs
@@ -32,7 +32,7 @@ namespace _9
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World! v2");
                 });
             });
         }
